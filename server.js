04e9b2bb0c65462c5f42e7c193f5a93e259cafe1
rana1997abdullah <rana1997abumazen@gmail.com>
2023-05-16 16:40:50 +0300

    upload files finish react cors app

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors')
-const app = express();
-app.use(cors());
-app.get('/greeting', (req, res) => {
-    res.json({ greeting: 'Hello' })
-})
-
-app.listen(5000, () => console.log('server has started'))
--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  fetch('http://localhost:5000/greeting').then(res =>
-    res.json()
-  ).then(data => console.log(data))
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const getTodos = async () => {
+    try {
+      const res = await axios.get("http://localhost:8080/todos/");
+      let data = res.data;
+      setTodos(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
+  const handleAdd = async () => {
+    await axios.post("http://localhost:8080/todos/", {
+      id: todos.length + 1,
+      title: title,
+    });
+    setTodos((todos) => [...todos, { id: todos.length + 2, title: title }]);
+  };
+  const handleDel = async (id) => {
+    await axios.delete(`http://localhost:8080/todos/${id}`);
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
   return (
     <div className="App">
-      app
+      <input
+        type="text"
+        name="title"
+        value={title}
+        className="input"
+        onChange={handleChange}
+      />
+      <button
+        disabled={title ? false : true}
+        onClick={handleAdd}
+        className="button"
+        style={{ backgroundColor: title ? "blueviolet" : "gray" }}
+      >
+        ADD
+      </button>
+      {todos?.map((todo) => {
+        return (
+          <div key={todo.id} className="itemdiv">
+            <li>
+              {todo.title} with id{todo.id}
+            </li>
+            <button className="button" onClick={() => handleDel(todo.id)}>
+              DELETE
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
